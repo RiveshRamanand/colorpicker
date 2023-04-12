8bd9a552bbf2d1af6a8cb0761f51e7d97ad3b019
RiveshRamanand <302376670@student.rocmondriaan.nl>
2023-04-12 14:59:03 +0200

    basis gemaakt

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ console.log("main.js loaded");
 
 // Variables
 const bgColor = {
-    r: 12, 
+    r: 12,
     g: 50,
     b: 100,
 
@@ -15,26 +15,52 @@ const bgColor = {
     toHex: function () {
         let result = "";
         // bewerk hier het resultaat naar een hexadecimaal getal. 
-        
+
         return result;
     }
 }
 
 const redSlider = document.querySelector("input.red");
+const greenSlider = document.querySelector("input.green");
+const blueSlider = document.querySelector("input.blue");
+
+const redValue = document.querySelector("span.r-value");
+const greenValue = document.querySelector("span.g-value");
+const blueValue = document.querySelector("span.b-value");
 
 //Event listeners
 redSlider.addEventListener("change", (event) => {
     bgColor.r = event.target.value;
     console.log("changing red: " + bgColor.r)
-    
+
     changeCircleColor();
+    
+    redValue.textContent = bgColor.r;
+});
+
+greenSlider.addEventListener("change", (event) => {
+    bgColor.g = event.target.value;
+    console.log("changing green: " + bgColor.g)
+
+    changeCircleColor();
+
+    greenValue.textContent = bgColor.g;
+});
+
+blueSlider.addEventListener("change", (event) => {
+    bgColor.b = event.target.value;
+    console.log("changing blue: " + bgColor.b)
+
+    changeCircleColor();
+
+    blueValue.textContent = bgColor.b;
 });
 
 
 //Functions
-function changeCircleColor(){
+function changeCircleColor() {
     const circleDiv = document.querySelector("div.circle");
-    circleDiv.style.backgroundColor = bgColor.toCSS(); 
-    
+    circleDiv.style.backgroundColor = bgColor.toCSS();
+
 }
 
